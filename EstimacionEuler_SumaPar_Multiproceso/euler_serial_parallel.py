13cb5e6d1fb62f8c2e1298cd437e0f08a7b55a41
Bearserker10029 <kentjianbin@gmail.com>
2025-05-10 18:40:58 -0500

    Update 2023-10-05 17:00:00.000000000 +0000

--- EstimacionEuler_SumaPar_Multiproceso/euler_serial_parallel.py
+++ EstimacionEuler_SumaPar_Multiproceso/euler_serial_parallel.py
@@ -3,7 +3,9 @@ from multiprocessing import Pool, cpu_count
 import time
 import matplotlib.pyplot as plt
 
-def e_serial(n:int):
+def e_serial(arg):
+    inicio, fin = arg
+    n = fin - inicio
     suma_total=0
     for _ in range(n):
         suma=0 
@@ -18,14 +20,21 @@ def e_serial(n:int):
     return estimado
 
 def e_paralelo(n, num_cpu):
-    rango=range(2,num_cpu+1)
+    rango=list(range(2,num_cpu+1))
 
     tiempo_paralelo=[]
-
+   
     for cpu in rango:
         inicio_paralelo=time.perf_counter()
+        rangos=[]
+        division=n//cpu
+        for i in range(cpu):
+            inicio = i * division
+            fin = (i + 1) * division if i < cpu - 1 else n
+            rangos.append((inicio, fin))
+
         with Pool(processes=cpu) as p:
-            resultado = p.map(e_serial,[n] * cpu)
+            resultado = p.map(e_serial, rangos)
             suma_paralela = sum(resultado)
         fin_paralelo=time.perf_counter()
         tiempo_paralelo.append(fin_paralelo-inicio_paralelo)
@@ -37,7 +46,7 @@ if __name__ == '__main__':
     n=5*pow(10,7)
     num_cpu = cpu_count()
     inicio=time.perf_counter()
-    estimado_serial=e_serial(n)
+    estimado_serial=e_serial((0,n))
     fin=time.perf_counter()
     tiempo_serial=fin-inicio
 
--- EstimacionEuler_SumaPar_Multiproceso/parallel_even_summation.py
+++ EstimacionEuler_SumaPar_Multiproceso/parallel_even_summation.py
@@ -2,19 +2,16 @@ from multiprocessing import Pool
 import time
 import matplotlib.pyplot as plt
 
-def sumatoria(n: int)->int:
-    n_par=0
+
+def sumatoria(args)->int:
+    f,n = args
     sumatoria=0
 
-    while n>0:
-        n_par=2*n
-        sumatoria+=n_par
+    while n>f:
+        sumatoria+=2*n
         n-=1
 
     return sumatoria
-
-def doble(i: int) -> int:
-    return 2 * i
 
 def paralelizar(n:int):
     proc_num=[4,8,16]
@@ -23,31 +20,40 @@ def paralelizar(n:int):
     
     for num in proc_num:        
         inicio_paralelo=time.perf_counter()
-        with Pool(processes=num) as p:
-            resultado = p.map(doble,range(1,n+1))
-            suma=sum(resultado)
-        fin_paralelo=time.perf_counter()
+        division=n//num
+        rangos=[]
 
-        suma_paralela.append(suma)
-        tiempo_paralelo.append(fin_paralelo-inicio_paralelo)
+        for i in range(num):
+            inicio = i * division
+            fin = (i + 1) * division if i < num - 1 else n
+            rangos.append((inicio, fin))
+
+        with Pool(processes=num) as p:
+            resultados = p.map(sumatoria, rangos)
+
+        suma_total = sum(resultados)
+        suma_paralela.append(suma_total)
+        fin_paralelo = time.perf_counter()
+        tiempo_paralelo.append(fin_paralelo - inicio_paralelo)
+
     return suma_paralela, tiempo_paralelo, proc_num
 
 if __name__ == '__main__':
     N=[pow(10,5),pow(10,6),5*pow(10,6),pow(10,7),5*pow(10,7),pow(10,8),pow(10,9)]
     for n in N:
         inicio_serial=time.perf_counter()
-        suma_serial=sumatoria(n)
+        suma_serial=sumatoria((0,n))
         fin_serial=time.perf_counter()
         tiempo_serial=fin_serial-inicio_serial
 
 
         suma_paralela, tiempo_paralelo, proc_num=paralelizar(n)
         
-        print(suma_paralela)
-        print (suma_serial)
+        #print (suma_paralela)
+        #print (suma_serial)
 
-
-        assert all(s == suma_serial for s in suma_paralela) and suma_serial == n*(n+1), "suma incorrecta"
+        for i in range(len(suma_paralela)):
+            assert suma_paralela[i] == suma_serial == n*(n+1), "suma incorrecta"
 
         print(f"numero {n}")
         print(f"Tiempo de ejecución serial: {tiempo_serial}")
